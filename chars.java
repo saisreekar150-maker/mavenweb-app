74cee9a87bdd52d848f9929922a963ff5cdc3911
sai.sreekar150 <sai.sreekar150@gmail.com>
2026-01-21 10:43:11 +0000

    deployed the changes

--- chars.java
+++ chars.java
@@ -1,10 +1,2 @@
-class Numbers {
-
-	public static void main(String [] args) {
-
-		for (int i=1; i<=5;i++) {
-			System.out.println(i);
-		}
-	}
-}
+pom.xml has been added into maven-web application
 
